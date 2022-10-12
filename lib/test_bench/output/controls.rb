9807242e04a6bcf0427c630981b5eb84ee7bcec3
Nathan Ladd <nathanladd@gmail.com>
2022-10-12 09:22:18 -0500

    Controls: Line Number

--- lib/test_bench/output/controls.rb
+++ lib/test_bench/output/controls.rb
@@ -3,3 +3,4 @@ require 'rbconfig'
 require 'test_bench/output/controls/random'
 
 require 'test_bench/output/controls/comment'
+require 'test_bench/output/controls/line_number'
---
+++ lib/test_bench/output/controls/line_number.rb
@@ -0,0 +1,10 @@
+module TestBench
+  module Output
+    module Controls
+      module LineNumber
+        def self.example = 11
+        def self.random = Random.integer
+      end
+    end
+  end
+end
